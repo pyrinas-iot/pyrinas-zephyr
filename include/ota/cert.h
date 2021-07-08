28e95f16597f8d259c931b00b4cd0832907b0d7a
Jared Wolff <hello@jaredwolff.com>
2021-07-07 20:49:57 -0400

    Updating to use the isrg-root-x1 rather than trusti-x3-root since it's expiring in September

--- include/ota/cert.h
+++ include/ota/cert.h
@@ -8,12 +8,7 @@
 
 /* Certificate for `LetsEncryptCA` */
 static const char pyrinas_ota_primary_cert[] = {
-#include "trustid-x3-root"
-};
-
-static const char pyrinas_ota_secondary_cert[] = {
 #include "isrg-root-x1"
 };
 
 BUILD_ASSERT(sizeof(pyrinas_ota_primary_cert) < KB(4), "Primary certificate too large");
-BUILD_ASSERT(sizeof(pyrinas_ota_primary_cert) < KB(4), "Secondary certificate too large");
